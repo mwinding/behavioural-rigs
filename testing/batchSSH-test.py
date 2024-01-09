6656b86522feb37dc19d40ec4e05ad89019ba93d
Michael Winding <michael.winding@crick.ac.uk>
2024-01-09 14:22:38 +0000

    added rig_num to connection error message

--- testing/batchSSH-test.py
+++ testing/batchSSH-test.py
@@ -52,7 +52,7 @@ for i, IP in enumerate(IPs):
     if result.returncode == 0:
         result = 1
     else:
-        print(f"Failed to connect to {IP}")
+        print(f"Failed to connect to {rig_num[i]} [{IP}]")
         result = 0
 
     IPs_connected.append([f'pc{rig_num[i]}', IP, result])
