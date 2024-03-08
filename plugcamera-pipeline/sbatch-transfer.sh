7254db1315c4bc4330b37df15dfcda6a3a310b82
Michael Winding <michael.winding@crick.ac.uk>
2024-03-08 22:48:14 +0000

    upgraded for user-input rig IDs

--- plugcamera-pipeline/sbatch-transfer.sh
+++ plugcamera-pipeline/sbatch-transfer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# usage: when transferring from plugcameras 50, 51, and 52 for example, use the following:
+# sbatch --export=RIG_NUMBERS="50 51 52" sbatch-transfer.sh
+
 #SBATCH --job-name=pc_transfer
 #SBATCH --ntasks=1
 #SBATCH --time=00:30:00
@@ -16,4 +19,5 @@ ml FFmpeg/4.1-foss-2018b
 source /camp/apps/eb/software/Anaconda/conda.env.sh
 
 conda activate plugcamera-pipeline
-python -u transfer-data.py -ip ip_addresses.csv -e test_exp -l 41 42 43 > python_output.log 2>&1
+#python -u transfer-data.py -ip ip_addresses.csv -e test_exp -l 41 42 43 > transfer_python-output.log 2>&1
+python -u transfer-data.py -ip ip_addresses.csv -e test_exp -l $RIG_NUMBERS > python_output.log 2>&1
