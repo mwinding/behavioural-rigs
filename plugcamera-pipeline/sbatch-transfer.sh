c7201f4471759d3f7452be549f343a36b95f76d9
Michael Winding <michael.winding@crick.ac.uk>
2024-03-08 15:58:20 +0000

    added ml FFmpeg

--- plugcamera-pipeline/sbatch-transfer.sh
+++ plugcamera-pipeline/sbatch-transfer.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=transfer-data
+#SBATCH --job-name=pc_transfer
 #SBATCH --ntasks=1
-#SBATCH --time=0:00:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=12G
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
 #SBATCH --output=slurm-%j.out
 #SBATCH --mail-user=$(whoami)@crick.ac.uk
 #SBATCH --mail-type=FAIL
 
 ml purge
 ml Anaconda3/2023.09-0
+ml FFmpeg/4.1-foss-2018b
 source /camp/apps/eb/software/Anaconda/conda.env.sh
 
 conda activate plugcamera-pipeline
-bash transfer-data.sh
+python transfer-data.py -ip ip_addresses.csv -e test_exp -l 41 42 43
