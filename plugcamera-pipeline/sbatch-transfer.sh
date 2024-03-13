da5f0736edb1f6b108b814e0fd05a3798cfa193e
Michael Winding <michael.winding@crick.ac.uk>
2024-03-13 08:45:25 +0000

    updated to externalise parameters

--- plugcamera-pipeline/sbatch-transfer.sh
+++ plugcamera-pipeline/sbatch-transfer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # usage: when transferring from plugcameras 50, 51, and 52 for example, use the following:
-# sbatch --export=EXP_NAME="test_exp" --export=RIG_NUMBERS="50 51 52" --export=REMOVE="False" sbatch-transfer.sh
+# sbatch --export=EXP_NAME=test_exp, RIG_NUMBERS="50 51 52", REMOVE=False sbatch-transfer.sh
 
 #SBATCH --job-name=pc_transfer
 #SBATCH --ntasks=1
-#SBATCH --time=00:30:00
+#SBATCH --time=08:00:00
 #SBATCH --mem=12G
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=8
@@ -20,4 +20,10 @@ source /camp/apps/eb/software/Anaconda/conda.env.sh
 
 conda activate plugcamera-pipeline
 #python -u transfer-data.py -ip ip_addresses.csv -e test_exp -l 41 42 43 > transfer_python-output.log 2>&1
-python -u transfer-data.py -ip ip_addresses.csv -e $EXP_NAME -l $RIG_NUMBERS -r $REMOVE > python_output.log 2>&1
+#python -u transfer-data.py -ip ip_addresses.csv -e $EXP_NAME -l $RIG_NUMBERS -r $REMOVE > python_output.log 2>&1
+
+# Convert RIG_NUMBERS into an array
+IFS=' ' read -r -a rig_numbers_array <<< "$RIG_NUMBERS"
+
+# Use the array expansion to pass individual numbers as separate arguments
+python -u transfer-data.py -ip ip_addresses.csv -e "$EXP_NAME" -l "${rig_numbers_array[@]}" -r "$REMOVE" > python_output.log 2>&1
