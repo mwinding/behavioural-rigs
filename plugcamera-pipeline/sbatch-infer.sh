4870e9f7ce767efe39a5d83d15156e483976588d
Michael Winding <michael.winding@crick.ac.uk>
2024-03-26 10:41:08 +0000

    updated to make more general

--- plugcamera-pipeline/sbatch-infer.sh
+++ plugcamera-pipeline/sbatch-infer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # usage: to run on pupae videos after unwrapping:
-# sbatch --export=EXP_NAME=test_exp,RIG_NUMBERS="50 51 52",REMOVE=False sbatch-transfer.sh
+# sbatch --export=VIDEOS_PATH=path/to/videos,CENTROID_PATH=path/to/model.centroid,CENTERED_PATH=path/to/model.centered_instance sbatch-infer.sh
 
-#SBATCH --job-name=SLEAP_training
+#SBATCH --job-name=SLEAP_infer
 #SBATCH --ntasks=1
 #SBATCH --time=08:00:00
 #SBATCH --mem=64G
@@ -20,6 +20,7 @@ source /camp/apps/eb/software/Anaconda/conda.env.sh
 conda activate sleap
 for video in "$VIDEOS_PATH"/*.jpg
 do
-  sleap-track "$video" -m 240306_235934.centroid -m 240306_235934.centered_instance
+  sleap-track "$video" -m "$CENTROID_PATH" -m "$CENTERED_PATH"
+  #sleap-track "$video" -m 240306_235934.centroid -m 240306_235934.centered_instance
   sleap-convert "$video".predictions.slp -o "$video".json --format json
 done
