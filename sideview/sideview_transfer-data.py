391fcb8eac2020087c7cd94c7c3d0d1e654917d0
Michael Winding <michael.winding@crick.ac.uk>
2025-02-14 10:34:35 +0000

    added job parameter, can either transfer data ('t'), convert to mp4 ('c'), or both ('tc')

--- sideview/sideview_transfer-data.py
+++ sideview/sideview_transfer-data.py
@@ -20,6 +20,7 @@ parser.add_argument('-c', '--condition', type=str, required=True, help='name of 
 parser.add_argument('-l', '--list-of-rig-names', nargs='+', type=int, default=[], help='list of rig names')
 parser.add_argument('-u', '--username', type=str, default=username, help='username for SSH attempts')
 parser.add_argument('-r', '--remove-files', action='store_true', help='whether to remove files from RPi source')
+parser.add_argument('-j', '--job', dest='job', action='store', type=str, default=None, help='t=transfer, c=convert to mp4')
 #parser.add_argument('-s', '--slurm-command', type=str, help='whether to remove files from RPi source')
 
 # ingesting user-input arguments
@@ -30,6 +31,8 @@ username = args.username
 experiment_name = args.experiment_name
 experiment_name_base = os.path.basename(experiment_name)
 condition = args.condition
+job = args.job
+
 #slurm_command = args.slurm_command
 
 remove_files = args.remove_files
@@ -63,123 +66,6 @@ if(len(list_names)>0):
 if not os.path.exists(save_path):
     os.makedirs(save_path, exist_ok=True)
 
-#########################
-#### TRANSFER DATA ######
-# transfer raw data from RPis to NEMO
-
-start_transfer = datetime.now()
-
-print(f'Running rsync on {len(IPs)} IPs, {IPs}') 
-IPs_string = ' '.join(IPs)
-rig_num_str = [f'sv{x}' for x in rig_num]
-rigs_string = ' '.join(rig_num_str)
-
-# remove source files from RPi or not, is a user-input parameter
-if remove_files==True: remove_files_option = '--remove-source-files '  
-if remove_files==False: remove_files_option = '' 
-
-print('\nremove_files variable:')
-print(remove_files_option)
-print('\n')
-
-if len(IPs) == 0:
-    print("No IP addresses found. Check your inventory.csv or filtering logic.")
-    exit(1)
-
-if len(IPs) == 1:
-    shell_script_content = f'''#!/bin/bash
-#SBATCH --job-name=rsync_pis
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --partition=ncpu
-#SBATCH --mem=120G
-#SBATCH --time=20:00:00
-
-# Single IP setup
-IFS=' ' read -r -a ip_array <<< "{IPs_string}"
-IFS=' ' read -r -a rig_array <<< "{rigs_string}"
-
-# Directly assign the single IP and rig
-ip="${{ip_array[0]}}"
-rig="${{rig_array[0]}}"
-
-echo "Job started at: $(date)"
-echo "Using IP: $ip and Rig: $rig"
-
-# rsync using the single IP address
-rsync -avzh --progress {username}@$ip:/home/{username}/data/ {save_path}
-rsync -avzh --progress {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
-rsync_status=$?
-
-# Check rsync status and output file if it fails
-if [ $rsync_status -ne 0 ]; then
-    echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
-fi
-
-ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
-ssh {username}@$ip "sudo shutdown -h now"
-'''
-
-
-elif len(IPs) > 1:
-    shell_script_content = f'''#!/bin/bash
-#SBATCH --job-name=rsync_pis
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --partition=ncpu
-#SBATCH --mem=120G
-#SBATCH --time=20:00:00
-#SBATCH --array=1-{len(IPs)}
-
-# Multiple IP setup
-IFS=' ' read -r -a ip_array <<< "{IPs_string}"
-IFS=' ' read -r -a rig_array <<< "{rigs_string}"
-
-# Get the IP and rig for the current task ID
-ip="${{ip_array[$SLURM_ARRAY_TASK_ID-1]}}"
-rig="${{rig_array[$SLURM_ARRAY_TASK_ID-1]}}"
-
-echo "Job started at: $(date)"
-echo "Using IP: $ip and Rig: $rig"
-
-# rsync using the IP address for this task
-rsync -avzh --progress {username}@$ip:/home/{username}/data/ {save_path}
-rsync -avzh --progress {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
-rsync_status=$?
-
-# Check rsync status and output file if it fails
-if [ $rsync_status -ne 0 ]; then
-    echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
-fi
-
-ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
-ssh {username}@$ip "sudo shutdown -h now"
-'''
-
-# Create a temporary file to hold the SBATCH script
-with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_script:
-    tmp_script.write(shell_script_content)
-    tmp_script_path = tmp_script.name
-
-# Submit the SBATCH script
-process = subprocess.run(["sbatch", tmp_script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-# Optionally, delete the temporary file after submission
-os.unlink(tmp_script_path)
-
-# Check the result and extract job ID from the output
-if process.returncode == 0:
-    job_id_output = process.stdout.strip()
-    print(job_id_output)
-
-    job_id = job_id_output.split()[-1]
-
-    print(process.stdout)
-else:
-    print("Failed to submit job")
-    print(process.stderr)
-    exit(1)
-    
 # Function to check if the array job is completed
 def is_job_array_completed(job_id):
     cmd = ["sacct", "-j", f"{job_id}", "--format=JobID,State", "--noheader"]
@@ -203,18 +89,136 @@ def is_job_array_completed(job_id):
 
     return all_completed
 
-seconds = 60
-print(f"Wait for {seconds} seconds before checking if array job has completed")
-time.sleep(seconds)
+#########################
+#### TRANSFER DATA ######
+# transfer raw data from RPis to NEMO
 
-# Wait for the array job to complete
-print(f"Waiting for array job {job_id} to complete...")
-while not is_job_array_completed(job_id):
-    print(f"Array job {job_id} is still running. Waiting...")
-    time.sleep(30)  # Check every 30 seconds
+start_transfer = datetime.now()
 
-print(f"Array job {job_id} has completed.\n")
-end_transfer = datetime.now()
+if 't' in job:
+    print(f'Running rsync on {len(IPs)} IPs, {IPs}') 
+    IPs_string = ' '.join(IPs)
+    rig_num_str = [f'sv{x}' for x in rig_num]
+    rigs_string = ' '.join(rig_num_str)
+
+    # remove source files from RPi or not, is a user-input parameter
+    if remove_files==True: remove_files_option = '--remove-source-files '  
+    if remove_files==False: remove_files_option = '' 
+
+    print('\nremove_files variable:')
+    print(remove_files_option)
+    print('\n')
+
+    if len(IPs) == 0:
+        print("No IP addresses found. Check your inventory.csv or filtering logic.")
+        exit(1)
+
+    if len(IPs) == 1:
+        shell_script_content = f'''#!/bin/bash
+    #SBATCH --job-name=rsync_pis
+    #SBATCH --ntasks=1
+    #SBATCH --cpus-per-task=16
+    #SBATCH --partition=ncpu
+    #SBATCH --mem=120G
+    #SBATCH --time=20:00:00
+
+    # Single IP setup
+    IFS=' ' read -r -a ip_array <<< "{IPs_string}"
+    IFS=' ' read -r -a rig_array <<< "{rigs_string}"
+
+    # Directly assign the single IP and rig
+    ip="${{ip_array[0]}}"
+    rig="${{rig_array[0]}}"
+
+    echo "Job started at: $(date)"
+    echo "Using IP: $ip and Rig: $rig"
+
+    # rsync using the single IP address
+    rsync -avzhP {username}@$ip:/home/{username}/data/ {save_path}
+    rsync -avzhP {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
+    rsync_status=$?
+
+    # Check rsync status and output file if it fails
+    if [ $rsync_status -ne 0 ]; then
+        echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
+    fi
+
+    ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
+    ssh {username}@$ip "sudo shutdown -h now"
+    '''
+
+
+    elif len(IPs) > 1:
+        shell_script_content = f'''#!/bin/bash
+    #SBATCH --job-name=rsync_pis
+    #SBATCH --ntasks=1
+    #SBATCH --cpus-per-task=16
+    #SBATCH --partition=ncpu
+    #SBATCH --mem=120G
+    #SBATCH --time=20:00:00
+    #SBATCH --array=1-{len(IPs)}
+
+    # Multiple IP setup
+    IFS=' ' read -r -a ip_array <<< "{IPs_string}"
+    IFS=' ' read -r -a rig_array <<< "{rigs_string}"
+
+    # Get the IP and rig for the current task ID
+    ip="${{ip_array[$SLURM_ARRAY_TASK_ID-1]}}"
+    rig="${{rig_array[$SLURM_ARRAY_TASK_ID-1]}}"
+
+    echo "Job started at: $(date)"
+    echo "Using IP: $ip and Rig: $rig"
+
+    # rsync using the IP address for this task
+    rsync -avzh --progress {username}@$ip:/home/{username}/data/ {save_path}
+    rsync -avzh --progress {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
+    rsync_status=$?
+
+    # Check rsync status and output file if it fails
+    if [ $rsync_status -ne 0 ]; then
+        echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
+    fi
+
+    ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
+    ssh {username}@$ip "sudo shutdown -h now"
+    '''
+
+    # Create a temporary file to hold the SBATCH script
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_script:
+        tmp_script.write(shell_script_content)
+        tmp_script_path = tmp_script.name
+
+    # Submit the SBATCH script
+    process = subprocess.run(["sbatch", tmp_script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Optionally, delete the temporary file after submission
+    os.unlink(tmp_script_path)
+
+    # Check the result and extract job ID from the output
+    if process.returncode == 0:
+        job_id_output = process.stdout.strip()
+        print(job_id_output)
+
+        job_id = job_id_output.split()[-1]
+
+        print(process.stdout)
+    else:
+        print("Failed to submit job")
+        print(process.stderr)
+        exit(1)
+
+    seconds = 60
+    print(f"Wait for {seconds} seconds before checking if array job has completed")
+    time.sleep(seconds)
+
+    # Wait for the array job to complete
+    print(f"Waiting for array job {job_id} to complete...")
+    while not is_job_array_completed(job_id):
+        print(f"Array job {job_id} is still running. Waiting...")
+        time.sleep(30)  # Check every 30 seconds
+
+    print(f"Array job {job_id} has completed.\n")
+    end_transfer = datetime.now()
 
 ###############################
 ###### PROCESS DATA ###########
@@ -223,93 +227,96 @@ end_transfer = datetime.now()
 
 start_processing = datetime.now()
 
-#### new bit using an array job to process the videos
-# Identify all .h264 files in the directory for array processing
-def list_directory_contents(folder_path):
-    # Check if the given path is a directory
-    if not os.path.isdir(folder_path):
-        print(f"{folder_path} is not a valid directory path.")
-        return
-    
-    # Get the list of items in the directory
-    contents = os.listdir(folder_path)
-    
-    return contents
+if 'c' in job:
+    #### new bit using an array job to process the videos
+    # Identify all .h264 files in the directory for array processing
+    def list_directory_contents(folder_path):
+        # Check if the given path is a directory
+        if not os.path.isdir(folder_path):
+            print(f"{folder_path} is not a valid directory path.")
+            return
+        
+        # Get the list of items in the directory
+        contents = os.listdir(folder_path)
+        
+        return contents
 
-directory_contents = list_directory_contents(save_path)
+    directory_contents = list_directory_contents(save_path)
 
-h264_files = [f"{save_path}/{file.replace('.h264', '')}" for file in directory_contents if file.endswith('.h264')]
-h264_files_string = ' '.join(h264_files)
+    h264_files = [f"{save_path}/{file.replace('.h264', '')}" for file in directory_contents if file.endswith('.h264')]
+    h264_files_string = ' '.join(h264_files)
 
-if len(h264_files) == 0:
-    print("No .h264 files found in the directory. Ensure the save_path is correct and contains files.")
-    exit(1)
+    if len(h264_files) == 0:
+        print("No .h264 files found in the directory. Ensure the save_path is correct and contains files.")
+        exit(1)
 
-# Array job script for processing each .h264 file
-process_script_content = f"""#!/bin/bash
-#SBATCH --job-name=sv_process
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --array=1-{len(h264_files)}
-#SBATCH --partition=ncpu
-#SBATCH --mem=120G
-#SBATCH --time=10:00:00
+    print(f'Running array job for mp4 conversion on:\n {h264_files_string}')
 
-# Convert h264_files_string to an array
-IFS=' ' read -r -a files <<< "{h264_files_string}"
-file="${{files[$SLURM_ARRAY_TASK_ID-1]}}"
+    # Array job script for processing each .h264 file
+    process_script_content = f"""#!/bin/bash
+    #SBATCH --job-name=sv_process
+    #SBATCH --ntasks=1
+    #SBATCH --cpus-per-task=32
+    #SBATCH --array=1-{len(h264_files)}
+    #SBATCH --partition=ncpu
+    #SBATCH --mem=120G
+    #SBATCH --time=10:00:00
 
-# Commands to process each file
-convert_mp4="ffmpeg -i \\"${{file}}.h264\\" -c:v copy -c:a copy \\"${{file}}_{condition}.mp4\\""
-convert_mp4_1fps="ffmpeg -i \\"${{file}}_{condition}.mp4\\" -vf 'fps=1' -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -c:a copy \\"${{file}}_{condition}_1fps.mp4\\""
-convert_mp4_30fps_playback="ffmpeg -i \\"${{file}}_{condition}_1fps.mp4\\" -filter:v 'setpts=PTS/24' -r 24 \\"${{file}}_{condition}_1fps_24fps-playback.mp4\\""
-remove_h264="rm \\"${{file}}.h264\\""
-remove_mp4=\\"${{file}}_{condition}_1fps.mp4\\""
+    # Convert h264_files_string to an array
+    IFS=' ' read -r -a files <<< "{h264_files_string}"
+    file="${{files[$SLURM_ARRAY_TASK_ID-1]}}"
 
-# Execute commands
-eval $convert_mp4
-eval $convert_mp4_1fps
-eval $convert_mp4_30fps_playback
-eval $remove_h264
-eval $remove_mp4
-"""
+    # Commands to process each file
+    convert_mp4="ffmpeg -i \\"${{file}}.h264\\" -c:v copy -c:a copy \\"${{file}}_{condition}.mp4\\""
+    convert_mp4_1fps="ffmpeg -i \\"${{file}}_{condition}.mp4\\" -vf 'fps=1' -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p -c:a copy \\"${{file}}_{condition}_1fps.mp4\\""
+    convert_mp4_30fps_playback="ffmpeg -i \\"${{file}}_{condition}_1fps.mp4\\" -filter:v 'setpts=PTS/24' -r 24 \\"${{file}}_{condition}_1fps_24fps-playback.mp4\\""
+    remove_h264="rm \\"${{file}}.h264\\""
+    remove_mp4=\\"${{file}}_{condition}_1fps.mp4\\""
 
-# Create a temporary file to hold the SBATCH script
-with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_process_script:
-    tmp_process_script.write(process_script_content)
-    tmp_process_script_path = tmp_process_script.name
+    # Execute commands
+    eval $convert_mp4
+    eval $convert_mp4_1fps
+    eval $convert_mp4_30fps_playback
+    eval $remove_h264
+    eval $remove_mp4
+    """
 
-# Submit the SBATCH script
-process_submission = subprocess.run(["sbatch", tmp_process_script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # Create a temporary file to hold the SBATCH script
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_process_script:
+        tmp_process_script.write(process_script_content)
+        tmp_process_script_path = tmp_process_script.name
 
-# Optionally, delete the temporary file after submission
-os.unlink(tmp_process_script_path)
+    # Submit the SBATCH script
+    process_submission = subprocess.run(["sbatch", tmp_process_script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-# Check the result and extract job ID from the output
-if process_submission.returncode == 0:
-    process_job_id_output = process_submission.stdout.strip()
-    print(process_job_id_output)
+    # Optionally, delete the temporary file after submission
+    os.unlink(tmp_process_script_path)
 
-    process_job_id = process_job_id_output.split()[-1]
+    # Check the result and extract job ID from the output
+    if process_submission.returncode == 0:
+        process_job_id_output = process_submission.stdout.strip()
+        print(process_job_id_output)
 
-    print(process_submission.stdout)
-else:
-    print("Failed to submit processing job array")
-    print(process_submission.stderr)
-    exit(1)
+        process_job_id = process_job_id_output.split()[-1]
 
-seconds = 60
-print(f"Wait for {seconds} seconds before checking if array job has completed")
-time.sleep(seconds)
+        print(process_submission.stdout)
+    else:
+        print("Failed to submit processing job array")
+        print(process_submission.stderr)
+        exit(1)
 
-print(f"Waiting for processing array job {process_job_id} to complete...")
-while not is_job_array_completed(process_job_id):
-    print(f"Processing array job {process_job_id} is still running. Waiting...")
-    time.sleep(30)  # Check every 30 seconds
+    seconds = 60
+    print(f"Wait for {seconds} seconds before checking if array job has completed")
+    time.sleep(seconds)
 
-print(f"Processing array job {process_job_id} has completed.\n")
+    print(f"Waiting for processing array job {process_job_id} to complete...")
+    while not is_job_array_completed(process_job_id):
+        print(f"Processing array job {process_job_id} is still running. Waiting...")
+        time.sleep(30)  # Check every 30 seconds
 
-end_processing = datetime.now()
+    print(f"Processing array job {process_job_id} has completed.\n")
+
+    end_processing = datetime.now()
 
 ################################
 ### HOW LONG DID IT TAKE? ######
