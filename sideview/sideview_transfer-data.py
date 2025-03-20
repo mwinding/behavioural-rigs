4a7fc16f3c97e7a878438324de3eafeee04649f7
Michael Winding <michael.winding@crick.ac.uk>
2025-03-20 13:33:11 +0000

    indented sbatch scripts

--- sideview/sideview_transfer-data.py
+++ sideview/sideview_transfer-data.py
@@ -115,73 +115,73 @@ if 't' in job:
 
     if len(IPs) == 1:
         shell_script_content = f'''#!/bin/bash
-    #SBATCH --job-name=rsync_pis
-    #SBATCH --ntasks=1
-    #SBATCH --cpus-per-task=16
-    #SBATCH --partition=ncpu
-    #SBATCH --mem=120G
-    #SBATCH --time=20:00:00
+        #SBATCH --job-name=rsync_pis
+        #SBATCH --ntasks=1
+        #SBATCH --cpus-per-task=16
+        #SBATCH --partition=ncpu
+        #SBATCH --mem=120G
+        #SBATCH --time=20:00:00
 
-    # Single IP setup
-    IFS=' ' read -r -a ip_array <<< "{IPs_string}"
-    IFS=' ' read -r -a rig_array <<< "{rigs_string}"
+        # Single IP setup
+        IFS=' ' read -r -a ip_array <<< "{IPs_string}"
+        IFS=' ' read -r -a rig_array <<< "{rigs_string}"
 
-    # Directly assign the single IP and rig
-    ip="${{ip_array[0]}}"
-    rig="${{rig_array[0]}}"
+        # Directly assign the single IP and rig
+        ip="${{ip_array[0]}}"
+        rig="${{rig_array[0]}}"
 
-    echo "Job started at: $(date)"
-    echo "Using IP: $ip and Rig: $rig"
+        echo "Job started at: $(date)"
+        echo "Using IP: $ip and Rig: $rig"
 
-    # rsync using the single IP address
-    rsync -avzhP {username}@$ip:/home/{username}/data/ {save_path}
-    rsync -avzhP {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
-    rsync_status=$?
+        # rsync using the single IP address
+        rsync -avzhP {username}@$ip:/home/{username}/data/ {save_path}
+        rsync -avzhP {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
+        rsync_status=$?
 
-    # Check rsync status and output file if it fails
-    if [ $rsync_status -ne 0 ]; then
-        echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
-    fi
+        # Check rsync status and output file if it fails
+        if [ $rsync_status -ne 0 ]; then
+            echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
+        fi
 
-    ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
-    ssh {username}@$ip "sudo shutdown -h now"
-    '''
+        ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
+        ssh {username}@$ip "sudo shutdown -h now"
+        '''
 
 
     elif len(IPs) > 1:
         shell_script_content = f'''#!/bin/bash
-    #SBATCH --job-name=rsync_pis
-    #SBATCH --ntasks=1
-    #SBATCH --cpus-per-task=16
-    #SBATCH --partition=ncpu
-    #SBATCH --mem=120G
-    #SBATCH --time=20:00:00
-    #SBATCH --array=1-{len(IPs)}
+        #SBATCH --job-name=rsync_pis
+        #SBATCH --ntasks=1
+        #SBATCH --cpus-per-task=16
+        #SBATCH --partition=ncpu
+        #SBATCH --mem=120G
+        #SBATCH --time=20:00:00
+        #SBATCH --array=1-{len(IPs)}
 
-    # Multiple IP setup
-    IFS=' ' read -r -a ip_array <<< "{IPs_string}"
-    IFS=' ' read -r -a rig_array <<< "{rigs_string}"
+        # Multiple IP setup
+        IFS=' ' read -r -a ip_array <<< "{IPs_string}"
+        IFS=' ' read -r -a rig_array <<< "{rigs_string}"
 
-    # Get the IP and rig for the current task ID
-    ip="${{ip_array[$SLURM_ARRAY_TASK_ID-1]}}"
-    rig="${{rig_array[$SLURM_ARRAY_TASK_ID-1]}}"
+        # Get the IP and rig for the current task ID
+        ip="${{ip_array[$SLURM_ARRAY_TASK_ID-1]}}"
+        rig="${{rig_array[$SLURM_ARRAY_TASK_ID-1]}}"
 
-    echo "Job started at: $(date)"
-    echo "Using IP: $ip and Rig: $rig"
+        echo "Job started at: $(date)"
+        echo "Using IP: $ip and Rig: $rig"
 
-    # rsync using the IP address for this task
-    rsync -avzh --progress {username}@$ip:/home/{username}/data/ {save_path}
-    rsync -avzh --progress {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
-    rsync_status=$?
+        # rsync using the IP address for this task
+        rsync -avzh --progress {username}@$ip:/home/{username}/data/ {save_path}
+        rsync -avzh --progress {remove_files_option}{username}@$ip:/home/{username}/data/ {save_path}
+        rsync_status=$?
 
-    # Check rsync status and output file if it fails
-    if [ $rsync_status -ne 0 ]; then
-        echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
-    fi
+        # Check rsync status and output file if it fails
+        if [ $rsync_status -ne 0 ]; then
+            echo "Rsync failed for IP: $ip" > "FAILED-rsync_{experiment_name_base}_${{rig}}_IP-${{ip}}.out"
+        fi
 
-    ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
-    ssh {username}@$ip "sudo shutdown -h now"
-    '''
+        ssh {username}@$ip "find data/ -mindepth 1 -type d -empty -delete"
+        ssh {username}@$ip "sudo shutdown -h now"
+        '''
 
     # Create a temporary file to hold the SBATCH script
     with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_script:
